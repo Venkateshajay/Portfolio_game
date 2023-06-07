90f527f60d912fc5bc1b796f71c3c41c94499d42
Venkateshajay <venkatesh23102003@gmail.com>
2023-06-07 21:08:08 +0530

    Added some panels in education canvas

--- Assets/Script/AboutMenu.cs
+++ Assets/Script/AboutMenu.cs
@@ -6,16 +6,19 @@ public class AboutMenu : MonoBehaviour
 {
     private UIManager uiManager;
     [SerializeField] Canvas UI;
+    [SerializeField] Canvas activeCanvas;
     //private bool isPlayerNeartheTrigger = false;
 
     private void Start()
     {
+        
         uiManager = FindObjectOfType<UIManager>().GetComponent<UIManager>();
     }
     private void OnTriggerEnter2D(Collider2D other)
     {
         if(other.tag == "Player")
         {
+            uiManager.currentlyActiveCanvas = activeCanvas;
             UI.enabled = true;
             uiManager.isPlayerNearTheTrigger = true;
             //isPlayerNeartheTrigger = true;
--- Assets/Script/UIManager.cs
+++ Assets/Script/UIManager.cs
@@ -8,6 +8,10 @@ public class UIManager : MonoBehaviour
     public bool isPlayerNearTheTrigger = false;
     [SerializeField] Button enterButton;
     public Canvas currentlyActiveCanvas;
+    [SerializeField] Image[] educationPanels;
+    [SerializeField] Image[] projectPanels;
+    private int educationPanelIndex=0;
+    private int projectPanelIndex=0;
     // Start is called before the first frame update
     public void TurnOnOffCanvas(bool OnorOff)
     {
@@ -24,4 +28,76 @@ public class UIManager : MonoBehaviour
         
     }
 
+    public void NextPanel(int indexOfPanel)
+    {
+        if (indexOfPanel == 0)
+        {
+            educationPanelIndex = IndexHandler(educationPanelIndex,educationPanels.Length,true);
+            TurnOnOffPanels(educationPanelIndex, educationPanels);
+        }
+        else if(indexOfPanel == 1)
+        {
+            projectPanelIndex = IndexHandler(projectPanelIndex, projectPanels.Length,true);
+            TurnOnOffPanels(projectPanelIndex, projectPanels);
+        }
+    }
+
+    private void TurnOnOffPanels(int index,Image[] array)
+    {
+        for(int i = 0; i < array.Length; i++)
+        {
+            array[index].gameObject.SetActive(true);
+            if (i == index)
+            {
+                continue;
+            }
+            else
+            {
+                array[i].gameObject.SetActive(false);
+            }
+        }
+    }
+
+    public void PreviousPanel(int indexOfPanel)
+    {
+        if (indexOfPanel == 0)
+        {
+            educationPanelIndex = IndexHandler(educationPanelIndex, educationPanels.Length, false);
+            TurnOnOffPanels(educationPanelIndex, educationPanels);
+        }
+        else if (indexOfPanel == 1)
+        {
+            projectPanelIndex = IndexHandler(projectPanelIndex, projectPanels.Length, false);
+            TurnOnOffPanels(projectPanelIndex, projectPanels);
+        }
+
+    }
+
+    private int IndexHandler(int index, int length,bool increment)
+    {
+        if (increment)
+        {
+            if (index < length)
+            {
+                index++;
+            }
+            if (index == length)
+            {
+                index = 0;
+            }
+        }
+        else
+        {
+            if (index >= 0)
+            {
+                index--;
+            }
+            if(index == -1)
+            {
+                index = length - 1;
+            }
+        }
+       
+        return index;
+    }
 }
